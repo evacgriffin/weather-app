8a40545cdae67e867e1e5c5d7ce95ac58a0071e1
Eva Griffin <eva@griffin.to>
2024-11-02 16:14:21 -0700

    Add Home, Create, Edit, and Delete pages

---
+++ WeatherApp/Models/SeedData.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WeatherApp.Data;
+
+namespace WeatherApp.Models;
+
+public static class SeedData
+{
+    public static void Initialize(IServiceProvider serviceProvider)
+    {
+        using (var context = new WeatherAppContext(
+            serviceProvider.GetRequiredService<
+                DbContextOptions<WeatherAppContext>>()))
+        {
+            if (context == null || context.DataPoint == null)
+            {
+                throw new ArgumentNullException("Null WeatherAppContext");
+            }
+
+            // Look for data points
+            if (context.DataPoint.Any())
+            {
+                return;   // DB has been seeded
+            }
+
+            context.DataPoint.AddRange(
+                new DataPoint
+                {
+                    Location = "Sammamish",
+                    Date = DateTime.Today,
+                    Time = DateTime.Now,
+                    Temperature = 58,
+                    Humidity = 74
+                },
+
+                new DataPoint
+                {
+                    Location = "Issaquah",
+                    Date = DateTime.Today,
+                    Time = DateTime.Now,
+                    Temperature = 56,
+                    Humidity = 74
+                },
+
+                new DataPoint
+                {
+                    Location = "Redmond",
+                    Date = DateTime.Today,
+                    Time = DateTime.Now,
+                    Temperature = 51,
+                    Humidity = 74
+                },
+
+                new DataPoint
+                {
+                    Location = "Seattle",
+                    Date = DateTime.Today,
+                    Time = DateTime.Now,
+                    Temperature = 61,
+                    Humidity = 83
+                }
+            );
+            context.SaveChanges();
+        }
+    }
+}
--- WeatherApp/Program.cs
+++ WeatherApp/Program.cs
@@ -1,6 +1,8 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using WeatherApp.Data;
+using WeatherApp.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -9,6 +11,20 @@ builder.Services.AddDbContext<WeatherAppContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("WeatherAppContext") ?? throw new InvalidOperationException("Connection string 'WeatherAppContext' not found.")));
 
 var app = builder.Build();
+
+// Configure default start page
+app.MapGet("/", async context =>
+{
+    context.Response.Redirect("/Home");
+});
+
+// Use seed initializer to seed the database
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+
+    SeedData.Initialize(services);
+}
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
