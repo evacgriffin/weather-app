2531c2a8671413983cfd63bfd99eb695581fc03e
Eva Griffin <eva@griffin.to>
2024-11-03 12:22:49 -0800

    Add filter feature

--- WeatherApp/Pages/Data/Index.cshtml.cs
+++ WeatherApp/Pages/Data/Index.cshtml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using WeatherApp.Data;
 using WeatherApp.Models;
@@ -21,9 +22,34 @@ namespace WeatherApp.Pages.Data
 
         public IList<DataPoint> DataPoint { get;set; } = default!;
 
+        [BindProperty(SupportsGet = true)]
+        public string? SearchString { get; set; }
+
+        public SelectList? Locations { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public string? DataLocation { get; set; }
+
         public async Task OnGetAsync()
         {
-            DataPoint = await _context.DataPoint.ToListAsync();
+            IQueryable<string> locationQuery = from d in _context.DataPoint
+                                            orderby d.Location
+                                            select d.Location;
+
+            var datapoints = from d in _context.DataPoint
+                         select d;
+
+            if (!string.IsNullOrEmpty(SearchString))
+            {
+                datapoints = datapoints.Where(s => s.Location.Contains(SearchString));
+            }
+
+            if (!string.IsNullOrEmpty(DataLocation))
+            {
+                datapoints = datapoints.Where(x => x.Location == DataLocation);
+            }
+            Locations = new SelectList(await locationQuery.Distinct().ToListAsync());
+            DataPoint = await datapoints.ToListAsync();
         }
     }
 }
